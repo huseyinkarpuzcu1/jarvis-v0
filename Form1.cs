a75cc35736db48a4f993f70c108e6827e63b8207
huseyinkarpuzcu1 <huseyinkarpuzcu371@gmail.com>
2023-05-15 14:23:20 +0300

    aaa

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace Jarvis_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer synt = new SpeechSynthesizer();
+        PromptBuilder pbuilder = new PromptBuilder();
+        SpeechRecognitionEngine Rengine = new SpeechRecognitionEngine();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Choices list = new Choices();
+            list.Add(new string[] { "Hello", "What time is it", "exit", "openchrome" });
+            Grammar gramer = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                Rengine.RequestRecognizerUpdate ();
+                Rengine.LoadGrammar(gramer);
+                Rengine.SpeechRecognized += Rengine_SpeechRecognized;
+                Rengine.SetInputToDefaultAudioDevice();
+                Rengine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception)
+            {
+
+                return;
+            }
+        }
+        void Rengine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "open chrome":
+                    System.Diagnostics.Process.Start("C:/Program Files/Google/Chrome/Application/chrome.exe");
+                    pbuilder.ClearContent();
+                    pbuilder.AppendText("Okey");
+                    synt.Speak(pbuilder);
+                    break;
+
+                case "exit":
+                    Application.Exit();
+                    break;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Rengine.Dispose();
+        }
+    }
+}
